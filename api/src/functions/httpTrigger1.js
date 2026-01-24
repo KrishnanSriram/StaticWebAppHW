c4e7a9ca2f457001868a08478db243683c005a4d
Krishnan Sriram <krishnan.sriram@hotmail.com>
2026-01-24 11:02:13 -0500

    Updated response from API

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -1,16 +1,19 @@
 const { app } = require('@azure/functions');
 
 app.http('httpTrigger1', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get('name') || (await request.text()) || 'world';
 
-        return { 'body': `Hello, ${name}!`,
-                'title': 'Greeting' };
-    }
+    return {
+      status: 200 /* Defaults to 200 */,
+      body: { body: `Hello, ${name}!`, title: 'Greeting' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  },
 });
-
-
